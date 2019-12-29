48c10bb95118843ccff46bbdb5364a8a78dfc754
Tapasvi <tapasvi.patel@mail.utoronto.ca>
2019-12-29 00:00:48 -0500

    Initial Commit.

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(int argc, char** argv) 
+{
+    vector<vector<int>> grid;
+    vector<vector<int>>::iterator gridItr;
+    
+    return 0;
+}
+
